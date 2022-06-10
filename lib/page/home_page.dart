4b5905e8bc3fec300d6c05c9ac3b16f790475621
Nicola <nicola.preda05@gmail.com>
2022-06-10 23:32:00 +0200

    Updated home page

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -67,10 +67,159 @@ class HomePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        Text('Rochambeau!',
-                            style: DefaultTextStyle.of(context)
-                                .style
-                                .apply(fontSizeFactor: 2.0)),
+                        Text(
+                          'Bentornato Mario!',
+                          style: DefaultTextStyle.of(context)
+                              .style
+                              .apply(fontSizeFactor: 2.0),
+                        ),
+                        Container(
+                            margin: EdgeInsets.only(top: 20),
+                            decoration: BoxDecoration(
+                                color: LIGHT_RED,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            child: Container(
+                                margin: EdgeInsets.all(20),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text("Dispensa",
+                                          style: DefaultTextStyle.of(context)
+                                              .style
+                                              .apply(fontSizeFactor: 1.5)),
+                                      SizedBox(height: 10),
+                                      Text(
+                                          "Il 56% dei prodotti in dispensa sta per scadere"),
+                                      SizedBox(height: 15),
+                                      LinearProgressIndicator(
+                                          value: 0.7,
+                                          backgroundColor: SECONDARY_LIGHT_RED,
+                                          color: PROGRESS_RED)
+                                    ]))),
+                        Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text("SCADENZE",
+                                      style: DefaultTextStyle.of(context)
+                                          .style
+                                          .apply(fontSizeFactor: 1)),
+                                  SizedBox(height: 10),
+                                  Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Container(
+                                          //set shadow
+                                          decoration: BoxDecoration(
+                                              color: LIGHT_RED,
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(20))),
+                                          padding: EdgeInsets.all(15),
+                                          child: Column(children: [
+                                            Image.asset(
+                                                'assets/images/test.png',
+                                                width: 50),
+                                            Text("Pan di stelle"),
+                                            Container(
+                                              padding: EdgeInsets.all(7),
+                                              decoration: BoxDecoration(
+                                                  color: PRIMARY_RED,
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(5))),
+                                              child: Text("20 FEB"),
+                                            )
+                                          ]),
+                                        ),
+                                        Container(
+                                          //set shadow
+                                          decoration: BoxDecoration(
+                                              color: LIGHT_RED,
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(20))),
+                                          padding: EdgeInsets.all(15),
+                                          child: Column(children: [
+                                            Image.asset(
+                                                'assets/images/test.png',
+                                                width: 50),
+                                            Text("Pan di stelle"),
+                                            Container(
+                                              padding: EdgeInsets.all(7),
+                                              decoration: BoxDecoration(
+                                                  color: PRIMARY_RED,
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(5))),
+                                              child: Text("20 FEB"),
+                                            )
+                                          ]),
+                                        ),
+                                        Container(
+                                          //set shadow
+                                          decoration: BoxDecoration(
+                                              color: LIGHT_RED,
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(20))),
+                                          padding: EdgeInsets.all(15),
+                                          child: Column(children: [
+                                            Image.asset(
+                                                'assets/images/test.png',
+                                                width: 50),
+                                            Text("Pan di stelle"),
+                                            Container(
+                                              padding: EdgeInsets.all(7),
+                                              decoration: BoxDecoration(
+                                                  color: PRIMARY_RED,
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(5))),
+                                              child: Text("20 FEB"),
+                                            )
+                                          ]),
+                                        )
+                                      ])
+                                ])),
+                        Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text("RISPARMIA",
+                                      style: DefaultTextStyle.of(context)
+                                          .style
+                                          .apply(fontSizeFactor: 1)),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                          padding: EdgeInsets.all(15),
+                                          child: Column(
+                                            children: [
+                                              Text("Esselunga"),
+                                              Container(
+                                                padding: EdgeInsets.all(7),
+                                                decoration: BoxDecoration(
+                                                    color: PRIMARY_RED,
+                                                    borderRadius:
+                                                        BorderRadius.all(
+                                                            Radius.circular(
+                                                                5))),
+                                                child: Text("20 FEB"),
+                                              )
+                                            ],
+                                          ))
+                                    ],
+                                  )
+                                ]))
                       ],
                     )))
           ],
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,3 +5,7 @@ import 'dart:ui';
 const PRIMARY_RED = Color.fromRGBO(255, 149, 139, 1);
 const SECONDARY_RED = Color.fromRGBO(255, 187, 181, 1);
 const BOTTOM_RED = Color.fromRGBO(255, 240, 240, 1);
+const LIGHT_RED = Color.fromRGBO(255, 238, 238, 1);
+const SECONDARY_LIGHT_RED = Color.fromRGBO(255, 231, 231, 1);
+const PROGRESS_RED = Color.fromRGBO(255, 195, 195, 1);
+const LIGHT_GRAY = Color.fromRGBO(251, 251, 251, 1);
